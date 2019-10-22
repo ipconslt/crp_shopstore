c13f908c0f36010eb634784cd9059d0bcc1ab58c
ipalakot <ipalakot@gmail.com>
2019-10-22 16:15:08 +0200

    Lancement de mes fixtures

--- src/DataFixtures/ArtcleFixtures.php
+++ src/DataFixtures/ArtcleFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Article;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -11,6 +12,17 @@ class ArtcleFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+       
+        for ($i=0; $i <=10 ; $i++) 
+        { 
+            $article = new Article();
+            $article->setTitle("Titre de l'article n°$i ")
+                    ->setContent("Contentu de l'article n°$i ")
+                    ->setImage("http://placehold.it/350x150")
+                    ->setCreatedAt(new \DateTime());
+
+                $manager->persist($article);
+        }
 
         $manager->flush();
     }
